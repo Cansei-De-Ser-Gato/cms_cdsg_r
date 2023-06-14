450e9818032dda523fe58adb5180325c94945d5f
eduvlemes <edu.vlemes@gmail.com>
2023-06-13 23:10:17 -0300

    store config

---
+++ src/api/store-config/controllers/store-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * store-config controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::store-config.store-config');
---
+++ src/api/store-config/routes/store-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * store-config router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::store-config.store-config');
